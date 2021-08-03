a48b2fa75e01f2304081e3f29e52b2d6edc59187
信翼 <wangkai.wang@alibaba-inc.com>
2021-08-03 16:56:52 +0800

    更新UMAPM 1.4.1 UMDevice 2.0.4

--- UMengComDemo/UMengFrameworks/UMAPM.framework/Headers/UMCrashConfigure.h
+++ UMengComDemo/UMengFrameworks/UMAPM.framework/Headers/UMCrashConfigure.h
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 typedef NSString *_Nullable(^CallbackBlock)(void);
+@class UMAPMConfig;
 
 @interface UMCrashConfigure : NSObject
 //获取sdk版本号
@@ -17,9 +18,16 @@ typedef NSString *_Nullable(^CallbackBlock)(void);
 + (void)setCrashCBBlock:(CallbackBlock _Nullable )cbBlock;
 
 
+/**
+ *  设置自定义版本号和build版本号即子版本号 (需要在初始化appkey方法之前调用)
+ */
++ (void)setAppVersion:( NSString * __nonnull )appVersion buildVersion:(NSString* __nullable)buildVersion;
 
 
-
+/**
+ *  设置APM的各个模块的开启/关闭配置(需要在初始化appkey方法之前调用)
+ */
++(void)setAPMConfig:(UMAPMConfig*_Nonnull)config;
 
 @end
 
